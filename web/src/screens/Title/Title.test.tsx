1074cc05f1662a6c2ca5cdaeda5c6376e447811c
kabeer11000 <kabeer@kabeersnetwork.tk>
2023-08-24 20:58:13 +0500

    blank bbranch so i can git rid of thiings

--- web/src/screens/Title/Title.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Title from './Title';
-
-it('It should mount', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Title />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
